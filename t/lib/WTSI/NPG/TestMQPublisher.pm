de87d03ce558537fd93591bdf93296b1eb3802c1
iainrb <ib5@sanger.ac.uk>
2017-08-14 15:19:19 +0100

    Add empty VERSION strings

--- t/lib/WTSI/NPG/TestMQPublisher.pm
+++ t/lib/WTSI/NPG/TestMQPublisher.pm
@@ -2,6 +2,8 @@ package WTSI::NPG::TestMQPublisher;
 
 use Moose;
 
+our $VERSION = '';
+
 extends 'WTSI::NPG::iRODS::Publisher';
 
 with qw[WTSI::NPG::iRODS::Reportable::PublisherMQ];
--- t/lib/WTSI/NPG/TestMQiRODS.pm
+++ t/lib/WTSI/NPG/TestMQiRODS.pm
@@ -2,6 +2,8 @@ package WTSI::NPG::TestMQiRODS;
 
 use Moose;
 
+our $VERSION = '';
+
 extends 'WTSI::NPG::iRODS';
 
 with qw[WTSI::NPG::iRODS::Reportable::iRODSMQ];
