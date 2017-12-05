280e6f2fdc1f05e0510a3f31c7e9f4af2652925d
iainrb <ib5@sanger.ac.uk>
2017-12-05 12:56:23 +0000

    Fix require test and test message

--- t/lib/WTSI/NPG/iRODS/PublisherFactoryTest.pm
+++ t/lib/WTSI/NPG/iRODS/PublisherFactoryTest.pm
@@ -15,7 +15,7 @@ Log::Log4perl::init('./etc/log4perl_tests.conf');
 use WTSI::NPG::iRODS::PublisherFactory;
 
 sub require : Test(1) {
-    require_ok('WTSI::NPG::iRODS::DataObject');
+    require_ok('WTSI::NPG::iRODS::PublisherFactory');
 }
 
 sub make_publishers : Test(3) {
@@ -35,6 +35,6 @@ sub make_publishers : Test(3) {
     $publisher = $factory->make_publisher($args);
     isa_ok($publisher, 'WTSI::NPG::iRODS::Publisher');
     ok(!($publisher->isa('WTSI::NPG::iRODS::PublisherWithReporting')),
-       'Reporting not enabled for Publisher');
+       'Factory does not return a PublisherWithReporting');
 
 }
